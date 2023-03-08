813dfbc765a59bfbaf91b6ffd4476723f02f4fc0
andrey <andreyskaskiv.py@gmail.com>
2023-03-08 21:06:18 +0200

    5. create PostDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from blog.views import PostListView
-
+from blog.views import PostListView, PostDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('page/<int:page>/', PostListView.as_view(), name='paginator'),
+
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>', PostDetailView.as_view(), name='post_detail')
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
@@ -8,3 +9,24 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
     queryset = Post.published.all()
     paginate_by = 2
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post/detail.html'
+    queryset = Post.published.all()
+
+    def get_object(self, queryset=None):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        slug = self.kwargs.get('slug')
+
+        queryset = queryset or self.queryset
+        post = get_object_or_404(queryset,
+                                 publish__year=year,
+                                 publish__month=month,
+                                 publish__day=day,
+                                 slug=slug)
+
+        return post
